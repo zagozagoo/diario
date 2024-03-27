4dbb890b5ed08ac8746c81261ada9eb4691e339e
Vitoria Zago <vivizago4884@gmail.com>
2024-03-27 14:48:11 -0300

    Ajusatndo acesso de portas

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,9 @@ const sequelize = require('sequelize');
 //configurações da base de dados   db       usuario   senha
 const database = new sequelize('Db_Diario', 'diario', 'ets@bosch123',
 {
-    dialect: 'mssql', host: 'localhost', port: 62232
+    dialect: 'mssql', host: 'localhost', port: 1433
+    // duda: 62232
+    // zago: 1433
 });
 
 database.sync();
