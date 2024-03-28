9373a00532d07472d67323935e735ef74bd16688
Maria Eduarda <silvamariaeduarda92@gmail.com>
2024-03-28 16:31:39 -0300

    Arrumado filtro de cursos da pagina inicial do administrador

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -12,34 +12,63 @@
 const curso = require('../model/curso');
 const turma = require('../model/turma');
 
+// module.exports = { // exporta o que ta dentro
 
+//     async pagInicialGet(req, res) {
+//         const cursos = await curso.findAll({
+//             raw: true,
+//             attributes: ['IDCurso', 'Nome']
+//         });
 
+//         res.render('../views/index', {cursos, turmas: '', id: ''});
 
-module.exports = { // exporta o que ta dentro
-
+//     },
+//     async pagInicialPost(req, res){
+//         const id = req.body.curso;
+//         const turmas = await turma.findAll({
+//         raw: true,
+//         attributes: ['IDTurma', 'Nome'],
+//         where: { IDCurso: id }
+//     });
+//     const cursos = await curso.findAll({ raw: true, attributes: ['IDCurso', 'Nome'] });
+//     res.render('../views/index', {cursos, turmas, id});
+//     }
+// }
+module.exports = {
     async pagInicialGet(req, res) {
         const cursos = await curso.findAll({
             raw: true,
             attributes: ['IDCurso', 'Nome']
         });
 
-        res.render('../views/index', {cursos, turma: '', id: ''});
-    
-        // const alunos = await aluno.findAll({
-        //     raw: true, // Retorna somente os valores de uma tabela, sem os metadados.
-        //     attributes: ['IDAluno', 'Nome', 'Idade']
-        // });
-        // res.render('../views/index', { salas, alunos });
-
-    },
-    async pagInicialPost(req, res){
-        const id = req.body.nome;
+        // Quando nenhum curso for selecionado, exiba todas as turmas
         const turmas = await turma.findAll({
-        raw: true,
-        attributes: ['IDTurma', 'Nome'],
-        where: { IDCurso: id }
-    });
-    const cursos = await curso.findAll({ raw: true, attributes: ['IDCurso', 'Nome'] });
-    res.render('../views/index', {cursos, turmas, id});
+            raw: true,
+            attributes: ['IDTurma', 'Nome']
+        });
+
+        res.render('../views/index', { cursos, turmas, id: '' });
+    },
+    async pagInicialPost(req, res) {
+        const id = req.body.curso;
+
+        let turmas;
+        if (id) {
+            // Se um curso for selecionado, exiba apenas as turmas desse curso
+            turmas = await turma.findAll({
+                raw: true,
+                attributes: ['IDTurma', 'Nome'],
+                where: { IDCurso: id }
+            });
+        } else {
+            // Se nenhum curso for selecionado, exiba todas as turmas
+            turmas = await turma.findAll({
+                raw: true,
+                attributes: ['IDTurma', 'Nome']
+            });
+        }
+
+        const cursos = await curso.findAll({ raw: true, attributes: ['IDCurso', 'Nome'] });
+        res.render('../views/index', { cursos, turmas, id });
     }
 }
