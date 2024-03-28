184e6239a7a8d5623bf96fa0a86b0cee678a171e
Vitoria Zago <vivizago4884@gmail.com>
2024-03-28 13:08:53 -0300

    Exemplo comentado em curso.js

--- src/controllers/curso.js
+++ src/controllers/curso.js
@@ -3,3 +3,18 @@ module.exports = {
         res.render('../views/curso');
     }
 }
+
+// EXEMPLO DO LUIZ
+// module.exports = {
+//     async pagProfessorGet(req, res) {
+//         // input no front tem name = 'nome_professor'
+//         prof = req.body.nome_professor
+
+//         const info_professor = await professores.findOne({
+//             raw: true,
+//             where: {'Nome': prof}
+//         });
+
+//         res.render('../views/professor', { info_professor });
+//     }
+// }
