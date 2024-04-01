41a7aea0a81104314ece0069673ff5c3b4f47210
Vitoria Zago <vivizago4884@gmail.com>
2024-04-01 16:44:00 -0300

    organizando javascript

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,13 @@
-//oi
+function toggleDarkMode() {
+    const body = document.body;
+    body.classList.toggle('dark-mode');
+}
+
+//caixa descricao dinamica
+const descricaoTextarea = document.getElementById('descricao');
+// adiciona um ouvinte de evento de entrada para a caixa de texto
+descricaoTextarea.addEventListener('input', function () {
+    // ajusta a altura da caixa de texto com base no conteúdo inserido pelo usuário
+    this.style.height = 'auto';
+    this.style.height = (this.scrollHeight) + 'px';
+});
