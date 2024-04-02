a93de43b73d413a5d2910032862f437133562608
Vitoria Zago <vivizago4884@gmail.com>
2024-04-02 16:06:18 -0300

    ajustes de login

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,28 +1,26 @@
+//const /*tabela no model*/ = require('../model/'/*dentro das aspas coloca a mesma tabela*/);
+
 module.exports = {
     async pagLoginGet(req, res) {
         res.render('../views/login');
     }
+    // ,
+    // async verificarLogin(req, res) {
+    //     const login = req.body.loginn;
+    //     const senha = req.body.senhaa;
+
+    //     const pessoaIdentificada = await pessoa.findOne ({
+    //         raw: true,
+    //         attributes: ['IDPessoa','Nome', 'EDV', 'Senha','Permissao'],
+    //         where: {EDV: login, Senha: senha}
+    //     });
+
+    //     if (pessoaIdentificada) {
+    //         res.redirect();
+    //     }
+    //     else {
+    //         resultado = false;
+    //         res.render('../views/login', {resultado});
+    //     }
+    //}
 }
-
-// const /*tabela no model*/ = require('../model/'/*dentro das aspas coloca a mesma tabela*/);
-
-// module.exports = {
-//     async verificarLogin(req, res) {
-//         const login = req.body.loginn;
-//         const senha = req.body.senhaa;
-
-//         const pessoaIdentificada = await pessoa.findOne ({
-//             raw: true,
-//             attributes: ['IDPessoa','Nome', 'EDV', 'Senha','Permissao'],
-//             where: {EDV: login, Senha: senha}
-//         });
-
-//         if (pessoaIdentificada) {
-//             res.redirect();
-//         }
-//         else {
-//             resultado = false;
-//             res.render('../views/login', {resultado});
-//         }
-//     }
-// }
